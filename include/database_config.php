a75ef7fa3da8e1661fff98d572f8b8ffd3d79b33
jamykapasi <91128310+jamykapasi@users.noreply.github.com>
2021-10-29 20:00:41 +0530

    Update database_config.php

--- include/database_config.php
+++ include/database_config.php
@@ -1,33 +1,38 @@
 <?php
     
-    // define("ENVIRONMENT", "d");
-    // define("DB_HOST", "localhost");
-    // define("DB_USER", "bhejooou_user");
-    // define("DB_PASS", "*U4Umy0UvDKD");
-    // define("DB_NAME", "bhejooou_db");
-    // define("PROJECT_DIRECTORY_NAME", "");
+    define("ENVIRONMENT", "d");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "bhejooo_portal");
+    define("DB_PASS", "9{b+C.__0PX(");
+    define("DB_NAME", "bhejooo_portal");
+    define("PROJECT_DIRECTORY_NAME", "");
         
-    // define("PROJECT_DIRECTORY_NAME_HEADER", "/");    
+    define("PROJECT_DIRECTORY_NAME_HEADER", "/app/");    
     
-    // define('SITE_URL', 'https://' . $_SERVER["SERVER_NAME"] . '/');
-    // define('ADMIN_URL', SITE_URL . 'admin/');
-    // define('DIR_URL', $_SERVER["DOCUMENT_ROOT"] . '/');
-    // error_reporting(0);
+    define('SITE_URL', 'https://' . $_SERVER["SERVER_NAME"] . '/app/');
+    define('ADMIN_URL', SITE_URL . 'admin/');
+    define('DIR_URL', $_SERVER["DOCUMENT_ROOT"] . '/app/');
+    error_reporting(0);
     
    // Local credensials
     
-    define("ENVIRONMENT", "d");
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "");  
-    define("DB_NAME", "digital");
-    define("PROJECT_DIRECTORY_NAME", "digital");
-    define("PROJECT_DIRECTORY_NAME_HEADER", "/digital/");
+    //define("ENVIRONMENT", "d");
+    //define("DB_HOST", "localhost");
+    //define("DB_USER", "root");
+    //define("DB_PASS", "");  
+    //define("DB_NAME", "digital");
+    //define("PROJECT_DIRECTORY_NAME", "digital");
+    //define("PROJECT_DIRECTORY_NAME_HEADER", "/digital/");
     
-    define('SITE_URL', 'http://' . $_SERVER["SERVER_NAME"] . '/digital/');
-    define('ADMIN_URL', SITE_URL . 'admin/');
-    define('DIR_URL', $_SERVER["DOCUMENT_ROOT"] . '/digital/');
-    error_reporting(E_ALL);
+    //define('SITE_URL', 'http://' . $_SERVER["SERVER_NAME"] . '/digital/');
+    //define('ADMIN_URL', SITE_URL . 'admin/');
+    //define('DIR_URL', $_SERVER["DOCUMENT_ROOT"] . '/digital/');
+    //error_reporting(E_ALL);
+    
+
+
+
+
     
 
 
