f1f53d8d51e78de99eb42a05798a6c10a0b16b71
jamykapasi <91128310+jamykapasi@users.noreply.github.com>
2021-10-30 17:00:52 +0530

    Create popup.php

---
+++ popup.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/css/all.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;500;600;700&amp;display=swap" rel="stylesheet">
+ 
+  <link rel="stylesheet" href="css/styles.css">
+   <link rel="stylesheet" href="css/style2.css">
+	
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+
+
+
+
+  
+  	
+<title>Popup</title>
+</head>
+<body class="overflow-page">
+	
+
+	
+	 <div class="col py-2 content popup" id="content">
+	 
+	<h3 class="main-heading">VIEW / EDIT ORDERS</h3>
+    <p class="desc ms-3 mt-2">Please preview your customer's address.
+    <br><br>
+    Fix it to avoid delay/failure in delivery.</p>
+	<div class="row custfields">
+	    
+	            <input type="text" name="cust_name" class="custinput col-5" placeholder="Customer Name *" required/>
+	            <input type="text" name="cust_phone" class="custinput col-5" placeholder=" Customer Phone*" required/>
+	            <input type="email"  name="cust_email" class="custinput col-5" placeholder="Customer Email Address">
+                 
+                <input type="text"  name="address" class="custinput col-10 wid" placeholder="Flat No. & Building Name*" required/>
+                
+                
+                <input type="text"  name="address2" class="custinput col-10 wid" placeholder="Road,Locality*" required>
+                <input type="text"  name="address3" class="custinput col-10 wid" placeholder="Landmark"></td>
+                <input type="text"  name="address4" class="custinput col-5" placeholder="Pincode*" required>
+                <input type="text"  name="address5" class="custinput col-5" placeholder="Area*" required>
+		<button class="btn2 btn-next update-btn btn-hv">UPDATE</button>
+	
+
+
+ <div>
+
+</div>
+
+
+</body>
+</html>
