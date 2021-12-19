76e076707518b4ac23e3251deefc552db93e7857
Ahmedtanwar <96001088+Ahmedtanwar@users.noreply.github.com>
2021-12-19 17:24:29 +0530

    Redirection Updated

--- api_call/install.php
+++ api_call/install.php
@@ -7,7 +7,7 @@ $shop = $_POST;
 
 $api_key = "076b1a79cdf5229219549b9e9a7cdbf8";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/digitotal/api_call/generate_token.php";
+$redirect_uri = "https://staging.bhejooo.com/app/api_call/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop['storeName'] . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
